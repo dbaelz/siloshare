55499b7eebabb8f5358b45a9ff1fb69ad603abed
Daniel Bälz <info@dbaelz.de>
2025-07-01 23:50:34 +0200

    Improve validation for blank text

--- src/main/kotlin/de/dbaelz/siloshare/controller/NoteController.kt
+++ src/main/kotlin/de/dbaelz/siloshare/controller/NoteController.kt
@@ -2,33 +2,24 @@ package de.dbaelz.siloshare.controller
 
 import de.dbaelz.siloshare.model.Note
 import de.dbaelz.siloshare.service.NoteService
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 data class CreateNoteRequest(
+    @field:NotBlank
     val text: String
 )
 
 @RestController
 @RequestMapping("/api/notes")
-@Validated
 class NoteController(
     private val noteService: NoteService
 ) {
     @PostMapping
-    fun add(@RequestBody request: CreateNoteRequest): ResponseEntity<Note> {
-        if (request.text.isBlank()) {
-            // TODO: Return error object instead?
-            return ResponseEntity.badRequest().body(
-                Note(
-                    id = "error",
-                    timestamp = java.time.Instant.now(),
-                    text = "text can not be empty"
-                )
-            )
-        }
+    fun add(@Valid @RequestBody request: CreateNoteRequest): ResponseEntity<Note> {
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(noteService.add(request.text))
     }
