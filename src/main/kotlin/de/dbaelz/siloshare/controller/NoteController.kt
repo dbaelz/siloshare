09303a4cf1bb9ec7861ce45dd73d988a45464f58
Daniel Bälz <info@dbaelz.de>
2025-07-01 23:00:45 +0200

    Add correct request body for POST request

--- src/main/kotlin/de/dbaelz/siloshare/controller/NoteController.kt
+++ src/main/kotlin/de/dbaelz/siloshare/controller/NoteController.kt
@@ -2,8 +2,13 @@ package de.dbaelz.siloshare.controller
 
 import de.dbaelz.siloshare.model.Note
 import de.dbaelz.siloshare.service.NoteService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+
+data class CreateNoteRequest(
+    val text: String
+)
 
 @RestController
 @RequestMapping("/api/notes")
@@ -11,8 +16,9 @@ class NoteController(
     private val noteService: NoteService
 ) {
     @PostMapping
-    fun add(@RequestBody text: String): ResponseEntity<Note> {
-        return ResponseEntity.ok(noteService.add(text))
+    fun add(@RequestBody request: CreateNoteRequest): ResponseEntity<Note> {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(noteService.add(request.text))
     }
 
     @GetMapping
